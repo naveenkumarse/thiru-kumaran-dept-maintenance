5e9b53bc50468ca2e182f2ba515162292eba7f93
Indira Kumar <indirakumarak@gmail.com>
2023-06-11 01:26:02 +0530

    <update> : fetch api for lineboy login

--- thiru-kumaran-dept/src/components/login/lineBoyLogin.js
+++ thiru-kumaran-dept/src/components/login/lineBoyLogin.js
@@ -1,40 +1,45 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Modal from '../modal/modal';
-// import jwt from 'jsonwebtoken';
-
+import { lineboyLogin } from '../../api';
+import jwtDecode from 'jwt-decode';
 const LineBoyLogin = () => {
     const navigate = useNavigate();
+    const [token, setToken] = useState('')
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [modalOpen, setModalOpen] = useState(false);
-    // just uncomment when u r using this
-    const validateJWT = (token) => {
-        // try {
-        //     const decoded = jwt.verify(token, 'your-secret-key');
-        //     // the format of decoded is similar to {
-        //     //   sub: "1234567890",
-        //     //   username: "John Doe",
-        //     //   iat: 1622055264
-        //     // } so use the below condition according to u
 
-        //     if (decoded.username == 'adminusername') {
-        //         navigate('/home');
-        //         localStorage.setItem("username",decoded.username);
-        //     }
-        //     console.log('Token is valid:', decoded);
-        //     return true;
-        // } catch (error) {
-            //setModalOpen(true);
-        //     console.error('Token is invalid:', error);
-        //     return false;
-        // }
+    const validateJWT = (token) => {
+        try {
+            const { decodedToken, isExpired } = jwtDecode(token);
+            console.log("decoded",decodedToken)
+            if (decodedToken.sub === email) {
+                navigate('/home');
+                localStorage.setItem("phoneNo",decodedToken.phoneNo);
+            }
+            console.log('Token is valid:', decodedToken);
+            return true;
+        } catch (error) {
+            setModalOpen(true);
+            console.error('Token is invalid:', error);
+            return false;
+        }
     };
     const onSubmit = (e) => {
         e.preventDefault();
-        // put the post call 
-        const token = '';
-        validateJWT(token);
+        // put the post call
+        const body = {
+            "phoneNo":email,
+            "password":password
+        } 
+        try {
+        lineboyLogin(body, setToken);
+            validateJWT(token)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
@@ -43,10 +48,10 @@ const LineBoyLogin = () => {
                 <div class="h-screen flex justify-center items-center">
                     <div class="bg-white mx-4 p-8 rounded shadow-md w-full md:w-1/2 lg:w-1/3">
                         <h1 class="text-3xl font-bold mb-8 text-center">Login</h1>
-                        <form onSubmit={onSubmit}>
+                        <form onSubmit={(e)=>onSubmit(e)}>
                             <div class="mb-4">
                                 <label class="block font-semibold text-gray-700 mb-2" for="email">
-                                    Username
+                                    Phone Number
                                 </label>
                                 <input
                                     class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -64,7 +69,7 @@ const LineBoyLogin = () => {
                             <div class="mb-6">
                                 <button
                                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                    type="button" >
+                                    type="submit" >
                                     Login
                                 </button>
                             </div>
